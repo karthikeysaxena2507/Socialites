6e2693337754f4cb2695ac3ed24edbd493b44e1e
Karthikey <karthikeysaxena@gmail.com>
2020-12-09 13:00:08 +0530

    deployed

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use("/posts", postsRouter);
 app.use("/users", usersRouter);
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static("../client/build"));
+    app.use(express.static("client/build"));
     const path = require("path");
     app.get("*", function(req,res) {
         res.sendFile(path.resolve(__dirname,"client","build","index.html"));
