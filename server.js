26acf0982d0a7e02c8ddaa09abafcf1c48edc1cb
Karthikey <karthikeysaxena@gmail.com>
2020-12-25 16:00:38 +0530

    updated code

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/socialites-karthikey/auth/google/social",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
