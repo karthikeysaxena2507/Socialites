db0e35b9004c623d3329e82303644f373647d363
Karthikey <karthikeysaxena@gmail.com>
2020-12-25 16:36:59 +0530

    updated code

--- server.js
+++ server.js
@@ -71,8 +71,7 @@ passport.use(new GoogleStrategy({
   }
 ));
 
-passport.use(new FacebookSTrategy(
-    {
+passport.use(new FacebookSTrategy({
       clientID: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
       callbackURL: process.env.FACEBOOK_CALLBACK_URL,
