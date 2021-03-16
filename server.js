3d972bf9be5bf19ad7ea7919b8260c3dc5930cf5
Karthikey <karthikeysaxena@gmail.com>
2021-03-16 17:54:19 +0530

    added cors

--- server.js
+++ server.js
@@ -8,14 +8,13 @@ const app = express();
 const server = http.createServer(app);
 const port = process.env.PORT || 5000;
 
+var corsOptions = {
+    origin: 'https://socialites.netlify.app/',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+
 // USING ALL MIDDLEWARES
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "https://socialites.netlify.app/");
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json( {limit: "50mb"}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
