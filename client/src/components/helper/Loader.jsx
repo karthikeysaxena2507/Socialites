11965b085669486d109d27d3dd1a3f79e7e3b43d
Karthikey <karthikeysaxena@gmail.com>
2021-03-17 13:20:45 +0530

    Fixed Bugs

--- client/src/components/helper/Loader.jsx
+++ client/src/components/helper/Loader.jsx
@@ -13,3 +13,25 @@ const Loader = () => {
 }
 
 export default Loader;
+
+// const Loader = () => {
+//     var style = {
+//         width: '150px',
+//         height: '100px',
+//         borderRadius: '4p',
+//         animation: 'move 1.2s ease-in-out infinite',
+//     }
+//     return (
+//         <div style={style}>
+//         <style>{`
+//             @keyframes move {
+//                 0%   {background-color: #ecf0f1;}
+//                 50%  {background-color: #e0e5e9;}
+//                 100% {background-color: #ecf0f1;}
+//             }
+//         `}</style>
+//         </div>
+//     )
+// }
+
+// export default Loader
